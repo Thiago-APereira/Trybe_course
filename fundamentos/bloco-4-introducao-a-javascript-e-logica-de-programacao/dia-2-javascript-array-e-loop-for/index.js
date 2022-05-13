bf91ece92dae50a621af18d4ea9306c5ee3176c1
Thiago Pereira <Star09Duster@hotmail.com>
2022-05-13 19:47:10 -0300

    ex 2 feito

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
@@ -1,4 +1,8 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-for(ind=0;ind < numbers.length;ind += 1){
-    console.log(numbers[ind])
+var resultado= 0; 
+var resultados=0;
+    for(var ind= 0;ind < numbers.length;ind++){
+       resultado = resultado+numbers[ind];
+       resultados = resultado/(numbers.length)
 }
+   console.log(resultados)
