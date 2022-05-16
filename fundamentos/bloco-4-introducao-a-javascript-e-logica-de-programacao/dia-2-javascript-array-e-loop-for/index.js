9792ea9899abb5ba36807e83b342b52c91d460d5
Thiago Pereira <Star09Duster@hotmail.com>
2022-05-16 15:33:18 -0300

    algo

--- fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
+++ fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-2-javascript-array-e-loop-for/index.js
@@ -1,13 +1,16 @@
 let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-var resultado= 0; 
+let resultado= []; 
 var resultados=0;
+;
     for(var ind= 0;ind < numbers.length;ind++){
-       resultado = resultado+numbers[ind];
-       resultados = resultado/(numbers.length+1)
+       switch (numbers[ind]>numbers[ind-1]&&numbers[ind+1]){
+       case: 
+           resultado.push(numbers[ind]);
+
+        }
+        default{
+console.log("deu ruim")
+           }
 }
-   if(resultados>20){
-       console.log("valor maior que 20")
-   }
-   else{
-       console.log("valor menor ou igual a 20");
-   }
+    
+console.log(resultado)
